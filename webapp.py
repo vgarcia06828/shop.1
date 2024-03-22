c71dcd48eb2f57f4cd3c996d2f347b7a1f93c5f0
vigarcia <vigarcia828@gmail.com>
2024-03-22 13:48:26 -0700

    shop3

--- webapp.py
+++ webapp.py
@@ -1,6 +1,19 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+
+
+item1 = "colorful shoes"
+item2 = "cargo pants"
+item3 = "long sleeve shirt"
+item4 = "short sleeve shirt"
+item5 = "black and white socks"
+item6 = "stylish hat"
+item7 = "necklace"
+item8 = "gold earrings"
+item9 = "silver ring" 
+
+
 
 @app.route("/")
 def render_main():
@@ -8,7 +21,11 @@ def render_main():
 
 @app.route("/p1")
 def render_page1():
-    return render_template('page1.html')
+    first = request.args['fname']
+    last = request.args['lname']
+    username = request.args['uname']
+    password = request.args['psw']
+    return render_template('page1.html',uname = username,psw = password)
 
 @app.route("/p2")
 def render_page2():
